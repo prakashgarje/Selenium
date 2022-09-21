07bc9ca943310ab634a8b221388adeee1d4790d4
prakashgarje <prakashgarje99@gmail.com>
2022-09-21 19:18:28 +0530

    export1

---
+++ Testing_CoreJava/src/com/nt/tests/Testxls.java
@@ -0,0 +1,5 @@
+package com.nt.tests;
+
+public class Testxls {
+Sheet
+}
---
+++ Testing_CoreJava/src/com/nt/tests/c2.java
@@ -0,0 +1,13 @@
+package com.nt.tests;
+
+public class c2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=100,b=200,c=300;
+		System.out.println("class 2");
+
+	}
+
+}
